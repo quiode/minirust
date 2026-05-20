fcab433386d857695f7c1eb2eb31241457792be1
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2026-05-20 21:22:46 +0200

    add failing tests from miri

---
+++ tooling/minimize/tests/ub/tree_borrows/implicit_writes/ptr_write_unsafe_cell.rs
@@ -0,0 +1,18 @@
+//@ compile-flags: --minirust-tree-borrows --minirust-implicit-writes
+
+// Copied from the Miri test suite and modified for MiniRust.
+// Same test as `ptr_write.rs` but with `UnsafeCell`.
+
+use std::cell::UnsafeCell;
+
+fn main() {
+    let mut x: UnsafeCell<u8> = 0u8.into();
+    let ptr = x.get();
+    let res = dereference(&mut x, ptr);
+    let _ = *res.get_mut();
+}
+
+fn dereference<T>(x: T, y: *mut u8) -> T {
+    let _ = unsafe { *y };
+    x
+}
